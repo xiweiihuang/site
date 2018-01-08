4c821928632f061f481afc4935bc920cb776880a
Phil Guo <phil@pku.edu.cn>
2018-01-08 02:39:58 -0800

    fixed section offset bug

--- js/page.js
+++ js/page.js
@@ -60,13 +60,13 @@ $(document).ready(function() {
       sectionOffset = $('.image-column').eq(0).offset().top - $('.content').eq(0).offset().top;
       anchorImage(elem, idx, true);
       imageOffsets[idx].height = elem.height();
-      imageOffsets[idx].top = elem.position().top;
+      // imageOffsets[idx].top = elem.position().top;
     });
     elem.children("video").eq(0).on('loadedmetadata', function() {
       sectionOffset = $('.image-column').eq(0).offset().top - $('.content').eq(0).offset().top;
       anchorImage(elem, idx, true);
       imageOffsets[idx].height = elem.height();
-      imageOffsets[idx].top = elem.position().top;
+      // imageOffsets[idx].top = elem.position().top;
     });
   });
 });
@@ -80,14 +80,20 @@ function anchorImage(elem, idx, verticalCenter) {
     return;
   }
 
-  var top = anchor.offset().top - $(".image-column").eq(0).offset().top;
   if (verticalCenter) {
     var anchorTextHeight = anchor.height();
     var imageHeight = elem.height();
     var verticalAlignmentAdjustment = (anchorTextHeight - imageHeight) / 2;
-    top += verticalAlignmentAdjustment;
+    var top = anchor.offset().top - $(".image-column").eq(0).offset().top + verticalAlignmentAdjustment;
+    elem.css('top', top);
+    imageOffsets[idx].top = top;
+    console.log("Update elem " + idx + " position:" + imageOffsets[idx].top);
+  } else {
+    var top = anchor.offset().top - $(".image-column").eq(0).offset().top;
+    elem.css('top', top);
+    imageOffsets[idx].top = top;
+    console.log("Update elem " + idx + " position:" + imageOffsets[idx].top);
   }
-  elem.css('top', top);
   elem.css('opacity', 1);
 }
 
@@ -120,8 +126,11 @@ function switchActive() {
     }
   }
 
-  if (nextActiveIdx != currentActiveIdx)
+  if (nextActiveIdx != currentActiveIdx) {
+    if (nextActiveIdx != -1)
+      console.log("made active: " + nextActiveIdx + ", scroll: " + (lastScrollTop - globalSnapAdjustment) + ", image top: " + (imageOffsets[nextActiveIdx].top + sectionOffset));
     makeActive(nextActiveIdx);
+  }
 }
 
 function makeActive(nextIdx) {
